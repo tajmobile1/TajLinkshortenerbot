d6b3fc2d245ff31f058dd28a343b693eea2834ea
tajmobile1 <114266082+tajmobile1@users.noreply.github.com>
2023-06-09 01:20:04 +0530

    Update users.py

--- database/users.py
+++ database/users.py
@@ -20,7 +20,7 @@ async def get_user(user_id):
             "header_text": "",
             "footer_text": "",
             "username": None,
-            "base_site": "shortnerfly.com",
+            "base_site": "tajlink.com",
             "banner_image": None,
             "is_banner_image": True,
             "is_username": True,
