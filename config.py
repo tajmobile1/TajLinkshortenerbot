475664f832743b0700d84619bde7bc3dbdb3e14f
tajmobile1 <114266082+tajmobile1@users.noreply.github.com>
2023-06-09 15:38:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
 ADMINS.append(Id Owned Id)
 #  Optionnal variables
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001944388469")) 
-UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "ajlinkofficial") # For Force Subscription
+UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "tajlinkofficial") # For Force Subscription
 BROADCAST_AS_COPY = os.environ.get('BROADCAST_AS_COPY', "True") # true if forward should be avoided
 WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '') # image when someone hit /start # image when someone hit /start
 LINK_BYPASS = "False"
